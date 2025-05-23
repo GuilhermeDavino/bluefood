0135bc6c5a1ff7abb85c84ba1f9113a2679ac0f8
Guilherme <guilherme.1009@hotmail.com>
2025-05-23 04:15:57 -0300

    PUT cidade

--- src/main/java/com/blue/bluefood/api/controller/CidadeController.java
+++ src/main/java/com/blue/bluefood/api/controller/CidadeController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +51,16 @@ public class CidadeController {
 					.buildAndExpand(cidade.getId())
 					.toUri();
 			return ResponseEntity.created(uri).body(cidade);
+		} catch(EntidadeNaoEncontradaException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> atualizar(@PathVariable Long id, @RequestBody Cidade cidade) {
+		try {
+			cidade = cidadeService.atualizar(id, cidade);
+			return ResponseEntity.ok(cidade);
 		} catch(EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
 		}
--- src/main/java/com/blue/bluefood/domain/service/CidadeService.java
+++ src/main/java/com/blue/bluefood/domain/service/CidadeService.java
@@ -1,5 +1,6 @@
 package com.blue.bluefood.domain.service;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,6 +28,19 @@ public class CidadeService {
 		
 		cidade = cidadeRepository.salvar(cidade);
 		return cidade;
+		
+	}
+	
+	public Cidade atualizar(Long id, Cidade cidade) {
+		Long estadoId = cidade.getEstado().getId();
+		Estado estado = estadoRepository.buscarPorId(estadoId);
+		if(estado == null) throw new EntidadeNaoEncontradaException(
+				String.format("O estado de id %d não existe ou não foi encontrado!", estadoId));
+		Cidade cidadeEntity = cidadeRepository.buscarPorId(id);
+		if(cidadeEntity == null) throw new EntidadeNaoEncontradaException(
+				String.format("A cidade de id %d não existe ou não foi encontrado!", id));
+		BeanUtils.copyProperties(cidade, cidadeEntity, "id");
+		return cidadeRepository.salvar(cidadeEntity);
 		
 	}
 }
